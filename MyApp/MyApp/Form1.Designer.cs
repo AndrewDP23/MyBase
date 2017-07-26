7cc37e0a99ae2b007c37cdb9ffb4fc3df3c41118
AndrewDP23 <githubcom@andrew.ac.nz>
2017-07-27 10:08:47 +1200

    Changed version # to 2

--- MyApp/MyApp/Form1.Designer.cs
+++ MyApp/MyApp/Form1.Designer.cs
@@ -36,9 +36,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(60, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.Size = new System.Drawing.Size(163, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Version 1";
+            this.label1.Text = "Version 2 (My Branched Change)";
             // 
             // Form1
             // 
